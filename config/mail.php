8e407cdac4312fac0e906e8410a6c995e1f21f6b
lucasbogo <lucasbogo93@gmail.com>
2022-05-25 17:34:07 -0300

    recuperar senha

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'lucasbogo93@gmail.com'), // remetente configurado
+        'name' => env('MAIL_FROM_NAME', 'Lucas'), // nome do remetente
     ],
 
     /*
