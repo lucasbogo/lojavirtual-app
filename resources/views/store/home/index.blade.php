f46d044ed14982be1e301949762b0e190592b1c0
lucasbogo <lucasbogo93@gmail.com>
2022-05-28 21:38:18 -0300

    atualizacao comentarios

--- resources/views/store/home/index.blade.php
+++ resources/views/store/home/index.blade.php
@@ -9,10 +9,13 @@
      
         <article class="col-md-3 col-sm-6 col-xm-12">
         <div class="product-item">
+            <!--exibir imagens localizadas em /images/temp de forma dinamica-->
             <img src="{{url("assets/images/temp/{$product->image}")}}" alt="" class="product-item-img">
         
+            <!--exibir nome dos produtos dinamicamente-->
             <h1>{{$product->name}}</h1>
         
+            <!--adiconar produto no carrinho-->
              <a href="{{route('add.cart', $product->id)}}" class="btn btn-buy">
                  Adicionar no Carrinho
                 <i class="fa fa-shopping-cart" aria-hidden="true"></i>
--- resources/views/store/templates/header.blade.php
+++ resources/views/store/templates/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
+  
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
@@ -8,18 +8,20 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+      <!--redirecionamento p/ home ao clickar na logo-->
       <a class="navbar-brand" href="{{route('store.home.index')}}">
         <img src="{{url('assets/images/Logotipo-01.png')}}" alt="NewModern" class="logo">
         </a>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       
       <ul class="nav navbar-nav navbar-right">
         <li>
-          <a href="{{route('store.cart.index')}}"><!--redirencionamento para carrinho da loja-->
+          <!--redirencionamento para carrinho da loja-->
+          <a href="{{route('store.cart.index')}}">
                 Meu Carrinho <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                <!--mostar quantidade itens dinamicamente no badge ao lado do ícone carrinho-->
                 <span class="badge">
                   @if (Session::has('cart'))
                       {{Session::get('cart')->totalItems()}}
@@ -30,25 +32,31 @@
                 </span>
             </a>
         </li>
-        @if( auth()->check() )<!--verificar se usuario está logado, se estiver mostra o dropdown-menu; se não mostra o link-login -->
+        <!--verificar se usuario está logado, se estiver mostra o dropdown-menu; se não mostra o link-login -->
+        @if( auth()->check() )
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            {{auth()->user()->name}} <!--Helper para exibir nome de usuário dinamicamente na view-->
+            <!--Helper para exibir nome de usuário dinamicamente na view-->
+            {{auth()->user()->name}} 
             <span class="caret"></span>
         </a>
         <ul class="dropdown-menu">
-          <li><a href="{{route('store.user.profile')}}">Perfil</a></li><!--redirecionamento para entrar no perfil-->
-          <li><a href="{{route('user.password')}}">Alterar Senha</a></li><!--redirencionamento para alterar senha-->
+          <!--redirecionamento para entrar no perfil-->
+          <li><a href="{{route('store.user.profile')}}">Perfil</a></li>
+          <!--redirencionamento para alterar senha-->
+          <li><a href="{{route('user.password')}}">Alterar Senha</a></li>
           <li role="separator" class="divider"></li>
-          <li><a href="{{route('store.user.logout')}}">Sair</a></li><!--redirencionamento para logout-->
+          <!--redirencionamento para logout-->
+          <li><a href="{{route('store.user.logout')}}">Sair</a></li>
           </ul>
         </li>
         @else
         <li>
+          <!--redirecionamento para Login ao clickar em Login-->
           <a href="{{route('login')}}">Login</a>
         </li>
         @endif
       </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+    </div>
+  </div>
 </nav>
